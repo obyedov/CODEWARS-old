0459c88c00beeb52f3fc6a2938f102f221b1f7da
obyedov <obyedov@gmail.com>
2022-08-01 22:26:13 +0200

    add first kata

---
+++ 01-8kata.js
@@ -0,0 +1,12 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+function opposite(number) {
+    return number > 0 || number  < 0 ? number * -1 : number
+  }
