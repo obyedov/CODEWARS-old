aca6c82421a090561f8041078da2d3d3cff5085d
obyedov <obyedov@gmail.com>
2022-08-11 20:32:35 +0200

    add 9 kata

---
+++ 09_8kata.js
@@ -0,0 +1,14 @@
+/**If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+ */
+var countSheep = function (num){
+    //your code here
+    let string = ''
+    for (let i = 0; i < num; i++) {
+      string += i + 1 +' sheep...'
+    }
+    return string
+  }
