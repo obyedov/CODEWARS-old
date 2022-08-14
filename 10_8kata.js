38fa109c756583b0497234e5adb448b238b2b605
obyedov <obyedov@gmail.com>
2022-08-14 21:33:14 +0200

    add 10 kata

---
+++ 10_8kata.js
@@ -0,0 +1,13 @@
+/**You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not. */
+function check(a, x) {
+    // your code here
+    let result = false;
+    for (let i = 0; i < a.length; i++) {
+     a[i] == x ? result = true : result
+    }
+    return result
+  }
